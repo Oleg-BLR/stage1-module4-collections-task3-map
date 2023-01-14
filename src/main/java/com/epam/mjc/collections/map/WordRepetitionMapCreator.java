0dd65880232f6935704cac1a8b42ab4328d45de0
Oleg <Oleg-BLR@yandex.ru>
2023-01-14 19:55:59 +0300

    Task 01_01

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,48 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.util.*;
 
 public class WordRepetitionMapCreator {
+
+
+    public static void main(String[] args) {
+
+//        String sent = "Fffg sss ff SSS hhhhhh sSs ddd ddd";
+        String sent = "Java map can store pairs of keys and values";
+        WordRepetitionMapCreator wordRepetitionMapCreator = new WordRepetitionMapCreator();
+        wordRepetitionMapCreator.createWordRepetitionMap(sent);
+    }
+
+
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> wordRepetitionMap = new TreeMap<>();
+//        wordRepetitionMap.put("sss", 5);
+//        String[] arrayOfStringElements = sentence.split(" ");
+        String[] arrayOfStringElements = sentence.split(" ");
+        System.out.println(Arrays.toString(arrayOfStringElements));
+
+//        for ()
+
+        for (int i = 0; i < arrayOfStringElements.length; i++) {
+//            if (!(wordRepetitionMap.containsKey(arrayOfStringElements[i].toLowerCase()))) {
+            if (!(wordRepetitionMap.containsKey((arrayOfStringElements[i]).toLowerCase()))) {
+                int countForValue = 1;
+                wordRepetitionMap.put((arrayOfStringElements[i]).toLowerCase(), countForValue);
+                for (int j = i + 1; j < arrayOfStringElements.length; j++) {
+
+
+                    if (arrayOfStringElements[i].equalsIgnoreCase(arrayOfStringElements[j])) {
+                        countForValue++;
+                    }
+                    wordRepetitionMap.put((arrayOfStringElements[i]).toLowerCase(), countForValue);
+//                    countForValue++;
+                }
+            }
+
+        }
+        System.out.println(wordRepetitionMap);
+
+        return wordRepetitionMap;
     }
 }
+
