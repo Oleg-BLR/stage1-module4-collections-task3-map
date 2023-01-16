54c881a0ad71a41166ebc933c52dca3d00d867a8
Oleg <Oleg-BLR@yandex.ru>
2023-01-16 16:01:25 +0300

    Task 01_03 three solutions, task 02, Task 03 raw

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,15 +1,36 @@
 package com.epam.mjc.collections.map;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class FunctionValueFinder {
+
+
+    public static void main(String[] args) {
+//    List<String> initialStringData = new ArrayList<>((Arrays.asList("7", "10", "6", "-7", "-2", "0", "-2", "-23")));
+        List<Integer> dataOfIntegers = new ArrayList<>(Arrays.asList(-1, 5, 4, 8));
+        FunctionValueFinder functionValueFinder = new FunctionValueFinder();
+//        functionValueFinder.isFunctionValuePresent02(dataOfIntegers, 1);
+//        System.out.println(functionValueFinder.isFunctionValuePresent02(dataOfIntegers, 1));
+        functionValueFinder.isFunctionValuePresent(dataOfIntegers, 28);
+        System.out.println(functionValueFinder.isFunctionValuePresent(dataOfIntegers, 28));
+
+    }
+
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+        int countRequiredValue = 0;
+        for (Integer v : functionMap.values()) {
+            if (v == requiredValue){
+                countRequiredValue++;
+            }
+        }
+        if (countRequiredValue > 0) return true;
         return false;
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
-        return null;
+        Map<Integer, Integer> calculatedMap = new HashMap<>();
+        sourceList.forEach(e -> calculatedMap.put(e, e * 5 + 2));
+        return calculatedMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,9 +1,52 @@
 package com.epam.mjc.collections.map;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class KeyValueSwapper {
+    public static void main(String[] args) {
+        Map<Integer, String> initialMap = new HashMap<>();
+        initialMap.put(1, "one");
+        initialMap.put(2, "two");
+        initialMap.put(3, "three");
+        initialMap.put(10, "one");
+        KeyValueSwapper keyValueSwapper = new KeyValueSwapper();
+        keyValueSwapper.swap(initialMap);
+
+    }
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
-        return null;
+        List<Integer> keyIntegerList = new ArrayList<>();
+        List<String> valueStringList = new ArrayList<>();
+        Map<String, Integer> swapedMap = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
+            keyIntegerList.add(entry.getKey());
+            valueStringList.add(entry.getValue());
+        }
+        for (int i = 0; i < valueStringList.size(); i++) {
+            String tempValue = valueStringList.get(i);
+            if (!(swapedMap.containsKey(tempValue))) {
+                int minKey = keyIntegerList.get(i);
+                for (int j = i + 1; j < valueStringList.size(); j++) {
+                    if (tempValue.equals(valueStringList.get(j))) {
+                        if (minKey > keyIntegerList.get(j)) {
+                            minKey = keyIntegerList.get(j);
+                        }
+                    }
+                }
+                swapedMap.put(tempValue, minKey);
+            }
+//        valueStringList.forEach(String );
+
+//        for (Integer key : sourceMap.keySet()) {
+//            sourceMap.get(key)
+//        }
+//            return swapedMap;
+        }
+        System.out.println(sourceMap);
+        System.out.println(keyIntegerList);
+        System.out.println(swapedMap);
+        return swapedMap;
     }
 }
